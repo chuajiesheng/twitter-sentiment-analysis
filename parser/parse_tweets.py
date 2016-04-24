845cd117f8b89baf2f498e8036f622d020ee5e43
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-04-24 13:39:48 +0800

    Add sentiment database and tweets

--- parser/parse_tweets.py
+++ parser/parse_tweets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import code
-from tweet import Tweet
+import tweet
 
 
 class InvalidFormatError(ValueError):
@@ -19,22 +19,68 @@ class TweetDatabase:
     @staticmethod
     def parse_line(line):
         if line is None: raise InvalidFormatError()
-
-        assert line is not None
         pair = line.split('\t')
         if len(pair) != 2: raise InvalidFormatError()
-
-        return Tweet(pair[0], pair[1])
+        return tweet.Tweet(pair[0], pair[1])
 
     def read_db(self):
         with open(self.db_path) as f:
             lines = [line.rstrip('\n') for line in f]
-        pairs = list(map(lambda l: TweetDatabase.parse_line(l), lines))
-        return pairs
+        tweets = list(map(lambda l: TweetDatabase.parse_line(l), lines))
+        return tweets
+
+
+class SentimentDatabase(TweetDatabase):
+
+    TASK_A_DEV_DB = 'data/semeval2016/dev/100_topics_100_tweets.sentence-three-point.subtask-A.dev.gold.txt'
+    dev_db_path = TASK_A_DEV_DB
+
+    def __init__(self, dev_db=TASK_A_DEV_DB, db_path=TweetDatabase.TWEET_DB):
+        TweetDatabase.__init__(self, db_path=db_path)
+        self.dev_db_path = dev_db
+
+    @staticmethod
+    def parse_labelled_tweet(line):
+        if line is None:
+            raise InvalidFormatError()
+
+        pair = line.split('\t')
+
+        if len(pair) != 2:
+            raise InvalidFormatError()
+
+        sid = pair[0]
+        sentiment = pair[1]
+        return {'sid': sid, 'sentiment': sentiment}
+
+    def read_dev_db(self):
+        with open(self.dev_db_path) as f:
+            lines = [line.rstrip('\n') for line in f]
+        dicts = list(map(lambda l: SentimentDatabase.parse_labelled_tweet(l), lines))
+        tweet_labels = {label['sid']: label['sentiment'] for label in dicts}
+        return tweet_labels
+
+    @staticmethod
+    def get_labelled_tweets(tweets, labels):
+        missing_label = 0
+        list_of_labelled_tweets = []
+        for t in tweets:
+            if t.sid not in labels.keys():
+                missing_label += 1
+                continue
+
+            list_of_labelled_tweets.append(tweet.SentimentTweet(t, labels[t.sid]))
+
+        print('Total missing labels: ', missing_label)
+        return list_of_labelled_tweets
+
 
 if __name__ == '__main__':
-    tweet_db = TweetDatabase()
-    pairs = tweet_db.read_db()
+    tweet_db = SentimentDatabase()
+    tweets = tweet_db.read_db()
+    labels = tweet_db.read_dev_db()
+    labelled_tweets = tweet_db.get_labelled_tweets(tweets, labels)
     code.interact(local=locals())
+
 
 
--- parser/tweet.py
+++ parser/tweet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from enum import Enum, unique
+
 
 class Tweet:
     sid = None
@@ -7,3 +9,23 @@ class Tweet:
     def __init__(self, sid, text):
         self.sid = sid
         self.text = text
+
+
+@unique
+class Sentiment(Enum):
+    negative = -1
+    neutral = 0
+    positive = 1
+
+
+class SentimentTweet(Tweet):
+    sentiment = Sentiment.neutral
+
+    def __init__(self, tweet, sentiment):
+        assert tweet is not None
+        assert sentiment is not None
+        assert sentiment in [s.name for s in Sentiment]
+
+        Tweet.__init__(self, tweet.sid, tweet.text)
+        self.sentiment = Sentiment[sentiment]
+
