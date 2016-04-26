7a33af08d71e15b6c8049ecf4ac40830179a2cc9
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-04-26 23:25:01 +0800

    Add logger

--- parser/parse_tweets.py
+++ parser/parse_tweets.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 import code
+import logging
 import tweet
+
+FORMAT = '[%(asctime)s][%(levelname)-8s] #%(funcName)-10s → %(message)s'
+logger = None
 
 
 class InvalidFormatError(ValueError):
@@ -36,13 +40,18 @@ class TweetDatabase:
         with open(self.db_path) as f:
             lines = [line.rstrip('\n') for line in f]
         tweets_arr = list(map(lambda l: TweetDatabase.parse_line(l), lines))
+        logging.info('Total records: %d', len(tweets_arr))
+
         tweets_dict = {tweet.sid: tweet for tweet in tweets_arr}
+        logging.info('Total unique records: %d', len(tweets_dict.items()))
         return tweets_dict
 
 if __name__ == '__main__':
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
     sdb = TweetDatabase()
     tweets = sdb.read_db()
-    code.interact(local=locals())
+    # code.interact(local=locals())
 
 
 
