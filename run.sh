8d37d37a617bea1927c55c5f8f51b8c893ee65d1
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-04-30 16:11:25 +0800

    Use relative path to home folder

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ set -x
 if [ "$SNAP_PIPELINE_COUNTER" == "" ]; then export SNAP_PIPELINE_COUNTER=dev; fi
 export PYTHONPATH=`pwd`
 
-python3 -m nltk.downloader -d /home/snap/nltk_data all
+python3 -m nltk.downloader -d ~/nltk_data all
 python3 analyzer/simple_naive_bayes.py
 python3 analyzer/vader.py
