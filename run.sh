87bc7aaa5d057be6a60747b9027cddbe1c110380
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-04-30 15:06:04 +0800

    Export pipeline counter and also download all nltk data

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -x
+
+if [ "$SNAP_PIPELINE_COUNTER" == "" ]; then export SNAP_PIPELINE_COUNTER=dev; fi
 export PYTHONPATH=`pwd`
+
+python3 -m nltk.downloader -d /home/snap/nltk_data all
 python3 analyzer/simple_naive_bayes.py
 python3 analyzer/vader.py
