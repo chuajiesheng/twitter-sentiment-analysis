fbc4904a3e4375ca9880447ab27589bb557187d1
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-05-01 02:02:23 +0800

    Add svm classifier

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ export PYTHONPATH=`pwd`
 python3 -m nltk.downloader -d ~/nltk_data all
 python3 analyzer/simple_naive_bayes.py
 python3 analyzer/multinomial_naive_bayes.py
+python3 analyzer/svm.py
 python3 analyzer/vader.py
