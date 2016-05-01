2f13c0a536d6b1459dc929a9b612bc009a810d51
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-05-01 17:39:49 +0800

    Chain run together

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ set -x
 if [ "$SNAP_PIPELINE_COUNTER" == "" ]; then export SNAP_PIPELINE_COUNTER=dev; fi
 export PYTHONPATH=`pwd`
 
-python3 -m nltk.downloader -d ~/nltk_data all
-python3 analyzer/simple_naive_bayes.py
-python3 analyzer/multinomial_naive_bayes.py
-python3 analyzer/svm.py
+python3 -m nltk.downloader -d ~/nltk_data all && \
+python3 analyzer/simple_naive_bayes.py && \
+python3 analyzer/multinomial_naive_bayes.py && \
+python3 analyzer/svm.py && \
 python3 analyzer/vader.py
