c4891b70a28d4f72757b76e6873d437e46e21428
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-05-04 01:30:00 +0800

    Rename sklearn to prevent naming conflict and add sklearn to run

--- run.sh
+++ run.sh
@@ -9,4 +9,8 @@ python3 -m nltk.downloader -d ~/nltk_data all && \
 python3 analyzer/simple_naive_bayes.py && \
 python3 analyzer/multinomial_naive_bayes.py && \
 python3 analyzer/svm.py && \
-python3 analyzer/vader.py
+python3 analyzer/vader.py && \
+python2 analyzer/sklearn_svm.py data/twitter > result/$SNAP_PIPELINE_COUNTER/sklearn.out && \
+python2 analyzer/sklearn_svm.py data/twitter_balanced > result/$SNAP_PIPELINE_COUNTER/sklearn_balanced.out && \
+true
+
