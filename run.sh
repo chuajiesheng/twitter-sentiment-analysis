4f8aaf0b757312c7f61e8d60b587a1a96ee7fb4a
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-05-07 23:46:34 +0800

    Add new neural network classifier from
    https://gist.github.com/nervanazoo/976ec931bb4549131ae0

--- run.sh
+++ run.sh
@@ -5,12 +5,21 @@ set -x
 if [ "$SNAP_PIPELINE_COUNTER" == "" ]; then export SNAP_PIPELINE_COUNTER=dev; fi
 export PYTHONPATH=`pwd`
 
+false && \
 python3 -m nltk.downloader -d ~/nltk_data all && \
 python3 analyzer/simple_naive_bayes.py && \
 python3 analyzer/multinomial_naive_bayes.py && \
 python3 analyzer/svm.py && \
 python3 analyzer/vader.py && \
-python2 analyzer/sklearn_svm.py data/twitter > result/$SNAP_PIPELINE_COUNTER/sklearn.out && \
+true
+
+false && \
+pip2 install sklearn && \
 python2 analyzer/sklearn_svm.py data/twitter_balanced > result/$SNAP_PIPELINE_COUNTER/sklearn_balanced.out && \
+true
+
+source activate neon && \
+python2 analyzer/recurrent_neural_network.py -e 2 -eval 1 --rlayer_type lstm && \
+source deactivate neon && \
 true
 
