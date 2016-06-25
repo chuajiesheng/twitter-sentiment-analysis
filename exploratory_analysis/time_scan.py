43732458a09c136cc64b0c1c46584c9ba1ed5300
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-06-25 23:02:06 +0800

    Return only english tweet and print the body of the tweet for analysis via other tools

--- exploratory_analysis/time_scan.py
+++ exploratory_analysis/time_scan.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     files = Reader.read_directory(working_directory)
     for f in files:
         tweets = Reader.read_file(f)
+        eng_tweets = filter(lambda t: t.language() == 'en', tweets)
         for tweet in tweets:
-            print '{}, {}'.format(tweet.verb(), tweet.timestamp())
+            print '{}, {}, {}'.format(tweet.verb(), tweet.timestamp(), tweet.body())
 
