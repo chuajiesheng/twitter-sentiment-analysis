b0d8299c00f37d853f03320ba35c28658cff3b93
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-07-17 04:56:47 +0800

    Add failed check count to print out at the end

--- step_1/scripts/instructional_sampling.py
+++ step_1/scripts/instructional_sampling.py
@@ -10,12 +10,16 @@ sys.setdefaultencoding('utf-8')
 # sc is an existing SparkContext.
 sqlContext = SQLContext(sc)
 
+failed_checks = 0
+
 directory = "/Volumes/JS'S FIT/gnip-json"
 datasets = sqlContext.read.json(directory)
 
 file_count = datasets.where(datasets['verb'].isNull()).count()
 # expecting 21888
 assert file_count == 21888
+if file_count != 21888:
+	failed_checks += 1
 
 info_dataset = datasets.select('info')
 info_dataset.registerTempTable('info')
@@ -26,20 +30,28 @@ all_posts = datasets.where(datasets['verb'] == 'post')
 all_posts_count = all_posts.count()
 # expecting 1570398
 assert all_posts_count == 1570398
+if all_posts_count != 1570398:
+	failed_checks += 1
 print '{} posts'.format(all_posts_count)
 
 all_shares = datasets.where(datasets['verb'] == 'share')
 all_shares_count = all_shares.count()
 # expecting 1112590
 assert all_shares_count == 1112590
+if all_shares_count != 1112590:
+	failed_checks += 1
 print '{} shares'.format(all_shares_count)
 
 assert all_tweets_count[0][0] == all_posts_count + all_shares_count
+if all_tweets_count[0][0] != all_posts_count + all_shares_count:
+	failed_checks += 1
 
 retweeted_post_ids = all_shares.select(all_shares['object.id'].alias('id')).rdd.map(lambda x: x.id).distinct()
 post_ids = all_posts.select('id').rdd.map(lambda x: x.id).distinct()
 keep_retweeted_post_ids = retweeted_post_ids.subtract(post_ids).collect()
 assert len(keep_retweeted_post_ids) < retweeted_post_ids.count()
+if len(keep_retweeted_post_ids) >= retweeted_post_ids.count():
+	failed_checks += 1
 
 from pyspark.sql.types import BooleanType
 exist_ = udf(lambda x: x in keep_retweeted_post_ids, BooleanType())
@@ -48,7 +60,11 @@ tweets_pool = all_posts.unionAll(all_shares.where(exist_(col('object.id')))).fil
 # check languages
 languages = tweets_pool.select('twitter_lang').distinct()
 assert languages.count() == 1
+if languages.count() != 1:
+	failed_checks += 1
 assert languages.first()['twitter_lang'] == 'en'
+if languages.first()['twitter_lang'] != 'en':
+	failed_checks += 1
 
 # validity check for tweets_pool
 all_posts_ids = post_ids.collect()
@@ -56,8 +72,12 @@ validity_1 = udf(lambda x: x not in all_posts_ids, BooleanType())
 validity_2 = udf(lambda x: x not in keep_retweeted_post_ids, BooleanType())
 invalid_tweets = tweets_pool.where(tweets_pool['verb'] == 'post').where(validity_1(col('id')))
 assert invalid_tweets.count() == 0
+if invalid_tweets.count() != 0:
+	failed_checks += 1
 invalid_tweets = tweets_pool.where(tweets_pool['verb'] == 'share').where(validity_2(col('object.id')))
 assert invalid_tweets.count() == 0
+if invalid_tweets.count() != 0:
+	failed_checks += 1
 
 tweets_pool_count = tweets_pool.count()
 tweets_pool_str_lengths = tweets_pool.select(length('body').alias('length')).rdd.map(lambda x: x.length).collect()
