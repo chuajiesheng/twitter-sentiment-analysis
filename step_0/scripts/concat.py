5002eeb3828e181215443317be57dcd4b2566757
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-07-17 02:11:51 +0800

    Do not nest the open file descriptors

--- step_0/scripts/concat.py
+++ step_0/scripts/concat.py
@@ -4,13 +4,17 @@ directory = './step_0/input'
 num_of_files = len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
 
 output_file = "./step_0/output/tweets.json"
-with open(output_file, 'w') as f:
-	for x in range(0, num_of_files):
-		filename = '{}/{}.json'.format(directory, x)
-		with open(filename) as j:
-			print 'Reading {}'.format(filename)
-			for line in j:
-				if len(line.strip()) < 1:
-					continue
-                f.write(line.strip())
-                f.write('\n')
+f = open(output_file, 'w')
+
+for x in range(0, num_of_files):
+	filename = '{}/{}.json'.format(directory, x)
+	with open(filename, 'r') as f2:
+		print 'Reading {}'.format(filename)
+		for line in f2:
+			if len(line.strip()) < 1:
+				continue
+			f.write(line.strip())
+			f.write('\n')
+			f.flush()
+
+f.close()
