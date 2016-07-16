16a5a01d1bbc015ab5b5e1357c1be08c51c31689
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-07-17 02:25:12 +0800

    Each output file have 10000 JSON file to prevent a single file being too big

--- step_0/scripts/concat.py
+++ step_0/scripts/concat.py
@@ -3,10 +3,27 @@ import os, os.path
 directory = './step_0/input'
 num_of_files = len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
 
-output_file = "./step_0/output/tweets.json"
-f = open(output_file, 'w')
+output_file_template = "./step_0/output/tweets_{}.json"
+current_file = 0
+file_per_set = 10000
+current_output_file = output_file_template.format(current_file)
+
+f = open(current_output_file, 'w')
+start = current_file * file_per_set
+end = start + file_per_set
 
 for x in range(0, num_of_files):
+
+	if x == end:
+		f.close()
+
+		current_file += 1
+		current_output_file = output_file_template.format(current_file)
+		f = open(current_output_file, 'w')
+
+		start = current_file * file_per_set
+		end = start + file_per_set
+
 	filename = '{}/{}.json'.format(directory, x)
 	with open(filename, 'r') as f2:
 		print 'Reading {}'.format(filename)
