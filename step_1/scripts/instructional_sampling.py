dfff82075ae6b8fbe61d67374fc3bd8da64a5896
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-07-17 16:07:24 +0800

    Add message among assertion

--- step_1/scripts/instructional_sampling.py
+++ step_1/scripts/instructional_sampling.py
@@ -30,6 +30,7 @@ file_count = datasets.where(datasets['verb'].isNull()).count()
 assert file_count == 21888
 if file_count != 21888:
     failed_checks += 1
+    log('[error] file_count = {}'.format(file_count))
 
 info_dataset = datasets.select('info')
 info_dataset.registerTempTable('info')
@@ -42,6 +43,7 @@ all_posts_count = all_posts.count()
 assert all_posts_count == 1570398
 if all_posts_count != 1570398:
     failed_checks += 1
+    log('[error] all_posts_count = {}'.format(all_posts_count))
 log('{} posts'.format(all_posts_count))
 
 all_shares = datasets.where(datasets['verb'] == 'share')
@@ -50,19 +52,24 @@ all_shares_count = all_shares.count()
 assert all_shares_count == 1112590
 if all_shares_count != 1112590:
     failed_checks += 1
+    log('[error] all_shares_count = {}'.format(all_shares_count))
 log('{} shares'.format(all_shares_count))
 
 assert all_tweets_count[0][0] == all_posts_count + all_shares_count
 if all_tweets_count[0][0] != all_posts_count + all_shares_count:
     failed_checks += 1
+    log('[error] all_tweets_count = {}'.format(all_tweets_count[0][0]))
 
 retweeted_post_ids = all_shares.select(all_shares['object.id'].alias('id')).rdd.map(lambda x: x.id).distinct()
 post_ids = all_posts.select('id').rdd.map(lambda x: x.id).distinct()
 keep_retweeted_post_ids = retweeted_post_ids.subtract(post_ids).collect()
 retweeted_post_ids_count = retweeted_post_ids.count()
+
 assert len(keep_retweeted_post_ids) < retweeted_post_ids_count
 if len(keep_retweeted_post_ids) >= retweeted_post_ids_count:
     failed_checks += 1
+    log('[error] keep_retweeted_post_ids = {}'.format(keep_retweeted_post_ids))
+    log('[error] retweeted_post_ids_count = {}'.format(retweeted_post_ids_count))
 
 exist_ = udf(lambda x: x in keep_retweeted_post_ids, BooleanType())
 tweets_pool = all_posts.unionAll(all_shares.where(exist_(col('object.id')))).filter("twitter_lang = 'en'")
@@ -73,10 +80,13 @@ languages_count = languages.count()
 assert languages_count == 1
 if languages_count != 1:
     failed_checks += 1
+    log('[error] languages_count = {}'.format(languages_count))
+
 language_retrieve = languages.first()
 assert language_retrieve['twitter_lang'] == 'en'
 if language_retrieve['twitter_lang'] != 'en':
     failed_checks += 1
+    log('[error] language_retrieve = {}'.format(language_retrieve['twitter_lang']))
 
 # validity check for tweets_pool
 all_posts_ids = post_ids.collect()
@@ -86,10 +96,13 @@ invalid_tweets_count = tweets_pool.where(tweets_pool['verb'] == 'post').where(va
 assert invalid_tweets_count == 0
 if invalid_tweets_count != 0:
     failed_checks += 1
+    log('[error] invalid_tweets_count = {}'.format(invalid_tweets_count ))
+
 invalid_tweets_count = tweets_pool.where(tweets_pool['verb'] == 'share').where(validity_2(col('object.id'))).count()
 assert invalid_tweets_count == 0
 if invalid_tweets_count != 0:
     failed_checks += 1
+    log('[error] invalid_tweets_count = {}'.format(invalid_tweets_count ))
 
 tweets_pool_count = tweets_pool.count()
 tweets_pool_str_lengths = tweets_pool.select(length('body').alias('length')).rdd.map(lambda x: x.length).collect()
@@ -97,9 +110,11 @@ lengths_np = np.array(tweets_pool_str_lengths)
 p = np.percentile(lengths_np, 20)
 final_tweets_pool = tweets_pool.filter(length('body') >= p)
 final_tweets_pool_count = final_tweets_pool.count()
-assert (float(final_tweets_pool_count) / tweets_pool_count) > 0.8
-if (float(final_tweets_pool_count) / tweets_pool_count) <= 0.8:
+percentage_kept = float(final_tweets_pool_count) / tweets_pool_count
+assert percentage_kept > 0.8
+if percentage_kept <= 0.8:
     failed_checks += 1
+    log('[error] percentage_kept = {}'.format(percentage_kept))
 
 sample_seed = 2016
 number_of_instructional_samples = 30
