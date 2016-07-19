e095dd959eadc52ff02ebb6ffb3008eefc7b2db6
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-07-20 02:05:13 +0800

    Do not filter share that is contained in post tweets
    Retweet (aka share) meant that the user want to show/represent the tweet that he/she was retweeting
    Also refactor assert into expect function

--- step_1/scripts/instructional_sampling.py
+++ step_1/scripts/instructional_sampling.py
@@ -58,93 +58,56 @@ log('# Completed reading JSON files')
 
 # Check checksum count
 file_count = datasets.where(datasets['verb'].isNull()).count()
-# expecting 21888
-assert file_count == 21888
-if file_count != 21888:
-    failed_checks += 1
-    log('[error] file_count = {}'.format(file_count))
-
-info_dataset = datasets.select('info')
-info_dataset.registerTempTable('info')
-all_tweets_count = info_dataset.select('info.activity_count').groupBy().sum('activity_count').collect()
-# expecting 2682988
+expect('file_count', file_count, 21888)
 
 # Check post count
 all_posts = datasets.where(datasets['verb'] == 'post')
 all_posts_count = all_posts.count()
-# expecting 1570398
-assert all_posts_count == 1570398
-if all_posts_count != 1570398:
-    failed_checks += 1
-    log('[error] all_posts_count = {}'.format(all_posts_count))
-log('{} posts'.format(all_posts_count))
+expect('all_posts_count', all_posts_count, 1570398)
 
 # Check share count
 all_shares = datasets.where(datasets['verb'] == 'share')
 all_shares_count = all_shares.count()
-# expecting 1112590
-assert all_shares_count == 1112590
-if all_shares_count != 1112590:
-    failed_checks += 1
-    log('[error] all_shares_count = {}'.format(all_shares_count))
-log('{} shares'.format(all_shares_count))
+expect('all_shares_count', all_shares_count, 1112590)
 
-assert all_tweets_count[0][0] == all_posts_count + all_shares_count
-if all_tweets_count[0][0] != all_posts_count + all_shares_count:
-    failed_checks += 1
-    log('[error] all_tweets_count = {}'.format(all_tweets_count[0][0]))
+# Check dataset count
+info_dataset = datasets.select('info')
+info_dataset.registerTempTable('info')
+all_tweets_count = info_dataset.select('info.activity_count').groupBy().sum('activity_count').collect()[0][0]
+expect('all_tweets_count', all_tweets_count, 2682988)
+expect('all_tweets_count', all_tweets_count, all_posts_count + all_shares_count)
 log('# Completed validating tweets count')
 
-retweeted_post_ids = all_shares.select(all_shares['object.id'].alias('id')).rdd.map(lambda x: x.id).distinct()
-post_ids = all_posts.select('id').rdd.map(lambda x: x.id).distinct()
-keep_retweeted_post_ids = retweeted_post_ids.subtract(post_ids).collect()
-retweeted_post_ids_count = retweeted_post_ids.count()
+# Remove post authored by @ChipotleTweet
+# GET https://api.twitter.com/1.1/users/show.json?screen_name=ChipotleTweets to get id
+chipotle_tweet = 'id:twitter.com:141341662'
+all_posts_wo_chipotle = all_posts.filter("actor.id != '{}'".format(chipotle_tweet))
+all_posts_w_chipotle_count = all_posts.filter("actor.id = '{}'".format(chipotle_tweet)).count()
+expect('all_posts_wo_chipotle', all_posts_wo_chipotle.count(), all_posts_count - all_posts_w_chipotle_count)
 
-assert len(keep_retweeted_post_ids) < retweeted_post_ids_count
-if len(keep_retweeted_post_ids) >= retweeted_post_ids_count:
-    failed_checks += 1
-    log('[error] keep_retweeted_post_ids = {}'.format(keep_retweeted_post_ids))
-    log('[error] retweeted_post_ids_count = {}'.format(retweeted_post_ids_count))
+# Remove share retweet of tweet by @ChipotleTweet
+all_shares_wo_chipotle = all_shares.filter("object.actor.id != '{}'".format(chipotle_tweet))
+all_shares_w_chipotle_count = all_shares.filter("object.actor.id = '{}'".format(chipotle_tweet)).count()
+expect('all_shares_wo_chipotle', all_shares_wo_chipotle.count(), all_shares_count - all_shares_w_chipotle_count)
 
-exist_ = udf(lambda x: x in keep_retweeted_post_ids, BooleanType())
-tweets_pool = all_posts.unionAll(all_shares.where(exist_(col('object.id')))).filter("twitter_lang = 'en'")
+# Generate tweets pool with only English tweet
+tweets_pool = all_posts_wo_chipotle.unionAll(all_shares_wo_chipotle).filter("twitter_lang = 'en'")
 tweets_pool.cache()
+tweets_pool_count = tweets_pool.count()
+expected_tweets_pool_count = all_posts_count - all_posts_w_chipotle_count + \
+                             all_shares_count - all_shares_w_chipotle_count
+expect('tweets_pool_count', tweets_pool_count, expected_tweets_pool_count)
 log('# Completed constructing tweets pool')
 
 # Check language of tweets
 languages = tweets_pool.select('twitter_lang').distinct()
 languages_count = languages.count()
-assert languages_count == 1
-if languages_count != 1:
-    failed_checks += 1
-    log('[error] languages_count = {}'.format(languages_count))
-
-language_retrieve = languages.first()
-assert language_retrieve['twitter_lang'] == 'en'
-if language_retrieve['twitter_lang'] != 'en':
-    failed_checks += 1
-    log('[error] language_retrieve = {}'.format(language_retrieve['twitter_lang']))
+language_check = languages.first()['twitter_lang']
+expect('languages_count', languages_count, 1)
+expect('language_check', language_check, 'en')
 log('# Completed validating language variety')
 
-# validity check for tweets_pool
-all_posts_ids = post_ids.collect()
-validity_1 = udf(lambda x: x not in all_posts_ids, BooleanType())
-validity_2 = udf(lambda x: x not in keep_retweeted_post_ids, BooleanType())
-invalid_tweets_count = tweets_pool.where(tweets_pool['verb'] == 'post').where(validity_1(col('id'))).count()
-assert invalid_tweets_count == 0
-if invalid_tweets_count != 0:
-    failed_checks += 1
-    log('[error] invalid_tweets_count = {}'.format(invalid_tweets_count ))
-
-invalid_tweets_count = tweets_pool.where(tweets_pool['verb'] == 'share').where(validity_2(col('object.id'))).count()
-assert invalid_tweets_count == 0
-if invalid_tweets_count != 0:
-    failed_checks += 1
-    log('[error] invalid_tweets_count = {}'.format(invalid_tweets_count ))
-log('# Completed checking duplicated tweets')
-
-tweets_pool_count = tweets_pool.count()
-log('{} tweets in tweets pool'.format(tweets_pool_count))
+# Take top 80% of tweets by length
 tweets_pool_str_lengths = tweets_pool.select(length('body').alias('length')).rdd.map(lambda x: x.length).collect()
 lengths_np = np.array(tweets_pool_str_lengths)
 p = np.percentile(lengths_np, 20)
@@ -155,10 +118,7 @@ tweets_pool.unpersist()
 
 final_tweets_pool_count = final_tweets_pool.count()
 percentage_kept = float(final_tweets_pool_count) / tweets_pool_count
-assert percentage_kept > 0.8
-if percentage_kept <= 0.8:
-    failed_checks += 1
-    log('[error] percentage_kept = {}'.format(percentage_kept))
+expect('percentage_kept', percentage_kept, 0.8, op=gt)
 log('# Completed sampling top 80% of tweets by body length')
 
 # Sampling
@@ -166,10 +126,10 @@ final_tweets_ids = final_tweets_pool.select(final_tweets_pool['id']).rdd.sortBy(
 
 # Sample tweets
 sample_seed = 2016
-number_of_instructional_samples = 200
-sample_posts = final_tweets_pool.select(final_tweets_pool['id']).rdd.sortBy(lambda x: x.id).map(lambda x: x.id).takeSample(False, number_of_instructional_samples, sample_seed)
+number_of_instructional_samples = 30
+sample_posts = final_tweets_ids.takeSample(False, number_of_instructional_samples, sample_seed)
 sample_posts_count = len(sample_posts)
-log('{} sample posts'.format(sample_posts_count))
+expect('sample_posts_count', sample_posts_count, number_of_instructional_samples)
 
 sample_posts_file = "sample_posts"
 sample_posts_jsons = final_tweets_pool[final_tweets_pool['id'].isin(sample_posts)].toJSON().collect()
@@ -181,9 +141,9 @@ log('# Completed exporting sample tweets')
 # Development tweets
 dev_seed = 20160717
 number_of_dev_samples = 3000
-dev_posts = final_tweets_pool.select(final_tweets_pool['id']).rdd.sortBy(lambda x: x.id).map(lambda x: x.id).takeSample(False, number_of_dev_samples, dev_seed)
+dev_posts = final_tweets_ids.takeSample(False, number_of_dev_samples, dev_seed)
 dev_posts_count = len(dev_posts)
-log('{} dev posts'.format(dev_posts_count))
+expect('dev_posts_count', dev_posts_count, number_of_dev_samples)
 
 dev_posts_file = "dev_posts"
 dev_posts_jsons = final_tweets_pool[final_tweets_pool['id'].isin(dev_posts)].toJSON().collect()
