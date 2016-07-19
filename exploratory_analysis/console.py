12519845ea1e74276261a5fb4f6b07fe3fb2f82c
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-07-19 09:35:17 +0800

    Set default encoding to utf-8

--- exploratory_analysis/console.py
+++ exploratory_analysis/console.py
@@ -1,8 +1,13 @@
 import os
 from utils import Reader
 import code
+import sys
 
 if __name__ == '__main__':
+    # coding=utf-8
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
     working_directory = os.getcwd()
     files = Reader.read_directory(working_directory)
     print '{} available data files'.format(len(files))
