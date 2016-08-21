dde30831e703bfc96c849ce97e0f803dcfcbd18d
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-08-21 19:39:50 +0800

    Change indentation format

--- step_0/scripts/concat.py
+++ step_0/scripts/concat.py
@@ -13,25 +13,24 @@ start = current_file * file_per_set
 end = start + file_per_set
 
 for x in range(0, num_of_files):
+    if x == end:
+        f.close()
 
-	if x == end:
-		f.close()
+        current_file += 1
+        current_output_file = output_file_template.format(current_file)
+        f = open(current_output_file, 'w')
 
-		current_file += 1
-		current_output_file = output_file_template.format(current_file)
-		f = open(current_output_file, 'w')
+        start = current_file * file_per_set
+        end = start + file_per_set
 
-		start = current_file * file_per_set
-		end = start + file_per_set
-
-	filename = '{}/{}.json'.format(directory, x)
-	with open(filename, 'r') as f2:
-		print 'Reading {}'.format(filename)
-		for line in f2:
-			if len(line.strip()) < 1:
-				continue
-			f.write(line.strip())
-			f.write('\n')
-			f.flush()
+    filename = '{}/{}.json'.format(directory, x)
+    with open(filename, 'r') as f2:
+        print 'Reading {}'.format(filename)
+        for line in f2:
+            if len(line.strip()) < 1:
+                continue
+            f.write(line.strip())
+            f.write('\n')
+            f.flush()
 
 f.close()
