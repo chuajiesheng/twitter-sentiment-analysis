513023944398ff6b47f9f675e54b7f45b239cd5b
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-11-24 19:00:36 +0800

    Take what is needed only

--- step_2/scripts/sample_subjectivity_tweets.py
+++ step_2/scripts/sample_subjectivity_tweets.py
@@ -186,20 +186,20 @@ log('# Completed constructing unsampled tweets')
 
 # Take top and bottom
 number_of_tweets_each = 1500
-positive_tweets = tweets_unsampled.orderBy(desc('score')).take(5000)
+positive_tweets = tweets_unsampled.orderBy(desc('score')).take(number_of_tweets_each)
 
 positive_tweet_file = "positive_tweets"
-positive_tweet_jsons = distinct_tweets_pool[distinct_tweets_pool['id'].isin(positive_tweets[:number_of_tweets_each])].toJSON().collect()
+positive_tweet_jsons = distinct_tweets_pool[distinct_tweets_pool['id'].isin(positive_tweets)].toJSON().collect()
 to_json(positive_tweet_file, positive_tweet_jsons)
 to_csv(positive_tweet_file, positive_tweet_jsons)
 log('Exporting positive tweets to {}'.format(positive_tweet_file))
 log('# Completed exporting positive tweets')
 
-negative_tweets = tweets_unsampled.orderBy(asc('score')).take(5000)
+negative_tweets = tweets_unsampled.orderBy(asc('score')).take(number_of_tweets_each)
 negative_tweet_ids = [t['id'] for t in negative_tweets]
 
 negative_tweet_file = "negative_tweets"
-negative_tweet_jsons = distinct_tweets_pool[distinct_tweets_pool['id'].isin(negative_tweet_ids[:number_of_tweets_each])].toJSON().collect()
+negative_tweet_jsons = distinct_tweets_pool[distinct_tweets_pool['id'].isin(negative_tweet_ids)].toJSON().collect()
 to_json(negative_tweet_file, negative_tweet_jsons)
 to_csv(negative_tweet_file, negative_tweet_jsons)
 log('Exporting negative tweets to {}'.format(negative_tweet_file))
