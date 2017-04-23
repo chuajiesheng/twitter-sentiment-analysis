36a3b5d28ede11de9fbbdbfe081879430c773a26
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-04-23 11:53:57 +0800

    Find the top 15 most retweet tweet

--- relevant_filtering/scripts/filter_relevant_tweets.py
+++ relevant_filtering/scripts/filter_relevant_tweets.py
@@ -1,62 +1,8 @@
-import csv
-import os
-import json
+import ast
 from collections import OrderedDict
 
 
-def get_sentiment_file(filename):
-    rows = []
-
-    with open(filename) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            if row['relevance'] == '1':
-                rows.append(row)
-
-    return rows
-
-# Read directory
-LABELLED_TWEETS = './relevant_filtering/input/tweets.csv'
-RELEVANT_TWEETS = './relevant_filtering/output/relevent_tweets.csv'
-
-rows = get_sentiment_file(LABELLED_TWEETS)
-print(len(rows))
-assert len(rows) == 610319
-
-if False:
-    with open(RELEVANT_TWEETS, 'w') as csvfile:
-        fieldnames = ['id', 'type', 'timestamp', 'body', 'Analytic', 'Clout', 'Authentic', 'Tone', 'affect', 'posemo',
-                      'negemo', 'anx', 'anger', 'sad', 'relevance', 'sentiment', 'mention']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for r in rows:
-            body = r['body']
-            parts = body.split()
-            for p in parts:
-                if p.startswith('@'):
-                    r['mention'] = '1'
-                else:
-                    r['mention'] = '0'
-
-            writer.writerow(r)
-
-tweet_ids = list(map(lambda row: row['id'], rows))
-assert len(tweet_ids) == 610319
-
-
-def read_directory(directory_name):
-    files = []
-    for (dirpath, dirnames, filenames) in os.walk(directory_name):
-        files.extend(filenames)
-        break
-
-    return files
-
-
 def read_file(filename):
-    is_tweet = lambda json_object: 'verb' in json_object and (json_object['verb'] == 'post' or json_object['verb'] == 'share')
-
     tweets = []
 
     with open(filename) as f:
@@ -64,25 +10,44 @@ def read_file(filename):
             if len(line.strip()) < 1:
                 continue
 
-            json_object = json.loads(line)
-            if is_tweet(json_object):
-                tweets.append(json_object)
-            else:
-                # this is a checksum line
-                activity_count = int(json_object['info']['activity_count'])
-                assert len(tweets) == activity_count
+            json_object = ast.literal_eval(line)
+            tweets.append(json_object)
 
     return tweets
 
 
-DIRECTORY = './relevant_filtering/input/tweets'
-convert_to_full_path = lambda p: '{}/{}'.format(DIRECTORY, p)
-files = list(map(convert_to_full_path, read_directory(DIRECTORY)))
-assert len(files) == 21888
+def add_to_sorted_dict(ordered_dict, retweet_count, tweet):
+    if len(ordered_dict) > 0:
+        ordered_dict = OrderedDict(sorted(ordered_dict.items(), key=lambda t: t[0], reverse=True))
 
+    current_min = min(ordered_dict, default=0)
+    if current_min >= retweet_count:
+        return ordered_dict
 
-for filename in files:
-    tweets = read_file(filename)
-    for t in tweets:
-        if t['id'] in tweet_ids:
-            print(t)
+    if len(ordered_dict) > 14:
+        last_item = ordered_dict.popitem()
+
+        min_count = min(ordered_dict)
+        assert min_count >= last_item[0]
+
+    assert(len(ordered_dict)) < 15
+
+    ordered_dict[retweet_count] = tweet
+
+    return ordered_dict
+
+RELEVANT_JSON = './relevant_filtering/output/relevent_tweets_json.txt'
+tweets = read_file(RELEVANT_JSON)
+
+tweet_parsed = 0
+top_10_tweets = OrderedDict()
+
+for t in tweets:
+    tweet_parsed += 1
+    top_10_tweets = add_to_sorted_dict(top_10_tweets, t['retweetCount'], t)
+
+assert tweet_parsed == 610319
+
+top_10_tweets = OrderedDict(sorted(top_10_tweets.items(), key=lambda t: t[0], reverse=True))
+for k in top_10_tweets.keys():
+    print(k, top_10_tweets[k]['id'])
