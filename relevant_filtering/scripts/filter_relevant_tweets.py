7617243c953a20c517a737c79fe0f54e55aef140
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-06-26 22:38:56 +0800

    Add by tweet id and sort by dict values

--- relevant_filtering/scripts/filter_relevant_tweets.py
+++ relevant_filtering/scripts/filter_relevant_tweets.py
@@ -1,47 +1,68 @@
 import ast
 from collections import OrderedDict
+import csv
+from utils import Reader
+import os
 
 
 def add_to_sorted_dict(ordered_dict, retweet_count, tweet):
-    if len(ordered_dict) > 0:
-        ordered_dict = OrderedDict(sorted(ordered_dict.items(), key=lambda t: t[0], reverse=True))
+    max_limit = 30
+    tweet_id = tweet.data['object']['id']
 
-    current_min = min(ordered_dict, default=0)
+    if tweet_id in ordered_dict.keys():
+        if ordered_dict[tweet_id] < retweet_count:
+            ordered_dict[tweet_id] = retweet_count
+        return ordered_dict
+
+    if len(ordered_dict) > 0:
+        ordered_dict = OrderedDict(sorted(ordered_dict.items(), key=lambda t: t[1], reverse=True))
+
+    current_min = min(ordered_dict.values(), default=0)
     if current_min >= retweet_count:
         return ordered_dict
 
-    if len(ordered_dict) > 14:
+    if len(ordered_dict) > (max_limit - 1):
         last_item = ordered_dict.popitem()
 
-        min_count = min(ordered_dict)
-        assert min_count >= last_item[0]
+        min_count = min(ordered_dict.values())
+        assert min_count >= last_item[1]
 
-    assert(len(ordered_dict)) < 15
+    assert(len(ordered_dict)) < max_limit
 
-    ordered_dict[retweet_count] = tweet
+    ordered_dict[tweet_id] = retweet_count
+    print('tweet', tweet_id, retweet_count)
 
     return ordered_dict
 
+RELEVANT_TWEETS = './relevant_filtering/input/relevent_tweets.csv'
+tweet_ids = []
+
+with open(RELEVANT_TWEETS) as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        tweet_ids.append(row['id'])
+
+assert(len(tweet_ids) == 610319)
+
+files = Reader.read_directory('./relevant_filtering/input/jsons')
+top_tweets = OrderedDict()
+
+for f in files:
+    file_path = os.path.join('./relevant_filtering/input/jsons', f)
+    tweets = Reader.read_file(file_path)
+    for t in tweets:
+        if t.is_post():
+            continue
+
+        if t.data['object']['id'] not in tweet_ids:
+            continue
+
+        top_tweets = add_to_sorted_dict(top_tweets, t.data['retweetCount'], t)
+
 RELEVANT_JSON = './relevant_filtering/output/relevent_tweets_json.txt'
-top_10_tweets = OrderedDict()
 
-with open(RELEVANT_JSON) as f:
-    for line in f:
-        if len(line.strip()) < 1:
-            continue
-
-        t = ast.literal_eval(line)
-
-        if t['object']['id'] == 'tag:search.twitter.com,2005:696116035502678016' \
-                or t['object']['id'] == 'tag:search.twitter.com,2005:685869482112241664' \
-                or t['object']['id'] == 'tag:search.twitter.com,2005:683100729498820608' \
-                or t['object']['id'] == 'tag:search.twitter.com,2005:609058908594958336':
-            continue
-
-        top_10_tweets = add_to_sorted_dict(top_10_tweets, t['retweetCount'], t)
-
-top_10_tweets = OrderedDict(sorted(top_10_tweets.items(), key=lambda t: t[0], reverse=True))
-for k in top_10_tweets.keys():
-    print(k, top_10_tweets[k]['id'])
+top_tweets = OrderedDict(sorted(top_tweets.items(), key=lambda t: t[1], reverse=True))
+for k in top_tweets.keys():
+    print(k, top_tweets[k])
 
 
